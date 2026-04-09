9689e4ec2ec216f75876f99fb9222e1d5d58efe3
Dula9812 <dulanjaomesh3@gmail.com>
2026-04-09 18:35:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.post('/convert', (req, res) => {
 
     const filename = `audio_${Date.now()}.mp3`;
 
-    const command = `yt-dlp -x --audio-format mp3 -o downloads/${filename} "${url}"`;
+    const command = `yt-dlp -x --audio-format mp3 --ffmpeg-location /usr/bin/ffmpeg -o downloads/${filename} "${url}"`;
 
     exec(command, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-            return res.json({ error: "Conversion failed" });
-        }
+		if (err) {
+		console.log("ERROR:", stderr);
+		return res.json({ error: "Conversion failed" });
+		}
 
         res.json({
             download: `https://YOUR-RENDER-URL/${filename}`
