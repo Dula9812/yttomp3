8a3e6c987241be05f402ed7a76f36f6da14e72fd
Dula9812 <dulanjaomesh3@gmail.com>
2026-04-13 17:44:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.post('/convert', (req, res) => {
     const filename = `audio_${Date.now()}.mp3`;
     const filepath = path.join(downloadsDir, filename);
 
-    // ✅ Use FULL PATH yt-dlp (important for Render)
-    const command = `/opt/render/.local/bin/yt-dlp "${url}" -x --audio-format mp3 -o "${filepath}" --no-playlist`;
+    const command = `/opt/render/.local/bin/yt-dlp "${url}" -f bestaudio --extract-audio --audio-format mp3 -o "${filepath}" --no-playlist`;
 
     exec(command, (err, stdout, stderr) => {
         console.log("STDOUT:", stdout);
